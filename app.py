d31587369126029a25ae9d785a20da1c113bbfb4
medinat <medinahakinsowon@gmail.com>
2024-06-06 18:05:10 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+def calculator(a , b):
+    sum = a + b
+    return sum
+
+result = calculator(2 , 5)
+print(result)
+
+
+
+def sumrange():
+    
+
+    sum = 0
+    
+    for num in range(2, 12):
+        sum += num
+
+        print(sum)
+
+print(sumrange())
+
+
+def sumtotal():
+    total = 0
+    for i in range(4, 20):
+       total += i
+
+       print(total)
+
+sumtotal()
+
